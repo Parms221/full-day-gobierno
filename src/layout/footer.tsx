da094459e219864aa181865924d5fbf4f259471f
JohannCastillo <johannjco15022@gmail.com>
2024-10-21 13:02:20 -0500

    mod: nueva propiedad de variantes y ejemplo de botones

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,14 +1,46 @@
 import { Button } from "@/components/ui/button";
-import { ChevronUp } from "lucide-react";
+import { ArrowRight, ArrowRightCircle, ChevronUp } from "lucide-react";
 
 export default function Footer() {
-    return (
-        <footer>
+  return (
+    <>
+      <div className="space-y-4">
+      <h1>Ejemplo botones</h1>
 
-            <Button className="btn-7" variant={"shine"}>
-                <ChevronUp />
-                HPLAAAAAAAA
-            </Button>
-        </footer>
-    )
+        <div className="grid grid-cols-4 gap-8">
+          <Button
+            className="text-secondary"
+            variant={"none"}
+            animation={"linkHover1"}
+          >
+            Button
+          </Button>
+          <Button
+            className="text-secondary"
+            variant={"none"}
+            animation={"linkHover2"}
+          >
+            Button
+          </Button>
+          <Button
+            className="text-secondary"
+            animation={"expandIcon"}
+            iconPlacement="right"
+            Icon={ArrowRightCircle}
+          >
+            Buton
+          </Button>
+          <Button
+            className="text-secondary"
+            animation={"gooeyRight"}
+            iconPlacement="right"
+            Icon={ArrowRightCircle}
+          >
+            Buton
+          </Button>
+        </div>
+      </div>
+      <footer className="p-8"></footer>
+    </>
+  );
 }
